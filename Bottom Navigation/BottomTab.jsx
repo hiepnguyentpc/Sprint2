f6334d91cc6d52bd466f887ba49922be278d0176
Hiep Nguyen <hiepnguyentpc@gmail.com>
2022-05-15 06:00:55 +0700

    Added Icons navigation bar

--- Bottom Navigation/BottomTab.jsx
+++ Bottom Navigation/BottomTab.jsx
@@ -4,12 +4,35 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../DemoScreens/HomeScreen';
 import UserScreen from '../DemoScreens/UserScreen';
 import ProductScreen from '../DemoScreens/ProductScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 const Tab = createBottomTabNavigator();
 
 const BottomTab = () => {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Product') {
+              iconName = focused ? 'ios-flame' : 'ios-flame';
+            } else if (route.name === 'User') {
+              iconName = focused ? 'bonfire-outline' : 'bonfire-outline';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+        
+        
+        >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Product" component={ProductScreen} />
         <Tab.Screen name="User" component={UserScreen} />
